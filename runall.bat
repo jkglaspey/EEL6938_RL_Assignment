ec433988b62f9f27cd359949075e24a674be51d6
jkglaspey <jkglaspey@gmail.com>
2025-03-16 04:17:08 -0400

    Organizing runall.bat

--- runall.bat
+++ runall.bat
@@ -30,6 +30,16 @@ python RL_assignment.py --model 0 --chunk_size 100 --learning_rate 3e-4 --batch_
 python RL_assignment.py --model 0 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 64,64 --reward 0
 python RL_assignment.py --model 0 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 128,128 --reward 0
 python RL_assignment.py --model 0 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 512,512 --reward 0
+python RL_assignment.py --model 0 --chunk_size 1 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
+python RL_assignment.py --model 0 --chunk_size 10 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
+python RL_assignment.py --model 0 --chunk_size 20 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
+python RL_assignment.py --model 0 --chunk_size 50 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
+python RL_assignment.py --model 0 --chunk_size 200 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
+python RL_assignment.py --model 0 --chunk_size 600 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
+python RL_assignment.py --model 0 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 1
+python RL_assignment.py --model 0 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 2
+python RL_assignment.py --model 0 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 3
+python RL_assignment.py --model 0 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 4
 python RL_assignment.py --model 1 --chunk_size 100 --learning_rate 1e-5 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef 0.0 --net_arch 256,256 --reward 0
 python RL_assignment.py --model 1 --chunk_size 100 --learning_rate 3e-5 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef 0.0 --net_arch 256,256 --reward 0
 python RL_assignment.py --model 1 --chunk_size 100 --learning_rate 1e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef 0.0 --net_arch 256,256 --reward 0
@@ -54,6 +64,7 @@ python RL_assignment.py --model 1 --chunk_size 100 --learning_rate 3e-4 --batch_
 python RL_assignment.py --model 1 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef 0.0 --net_arch 64,64 --reward 0
 python RL_assignment.py --model 1 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef 0.0 --net_arch 128,128 --reward 0
 python RL_assignment.py --model 1 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef 0.0 --net_arch 512,512 --reward 0
+python RL_assignment.py --model 1 --chunk_size 200 --learning_rate 3e-4 --batch_size 128 --gamma 0.9 --ent_coef 0.0 --net_arch "64,64" --reward 4
 python RL_assignment.py --model 2 --chunk_size 100 --learning_rate 1e-5 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
 python RL_assignment.py --model 2 --chunk_size 100 --learning_rate 3e-5 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
 python RL_assignment.py --model 2 --chunk_size 100 --learning_rate 1e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
@@ -110,14 +121,3 @@ python RL_assignment.py --model 3 --chunk_size 100 --learning_rate 3e-4 --batch_
 python RL_assignment.py --model 3 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 64,64 --reward 0
 python RL_assignment.py --model 3 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 128,128 --reward 0
 python RL_assignment.py --model 3 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 512,512 --reward 0
-python RL_assignment.py --model 0 --chunk_size 1 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
-python RL_assignment.py --model 0 --chunk_size 10 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
-python RL_assignment.py --model 0 --chunk_size 20 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
-python RL_assignment.py --model 0 --chunk_size 50 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
-python RL_assignment.py --model 0 --chunk_size 200 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
-python RL_assignment.py --model 0 --chunk_size 600 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 0
-python RL_assignment.py --model 0 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 1
-python RL_assignment.py --model 0 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 2
-python RL_assignment.py --model 0 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 3
-python RL_assignment.py --model 0 --chunk_size 100 --learning_rate 3e-4 --batch_size 256 --buffer_size 200000 --tau 0.005 --gamma 0.99 --ent_coef -1.0 --net_arch 256,256 --reward 4
-python RL_assignment.py --model 1 --chunk_size 200 --learning_rate 3e-4 --batch_size 128 --gamma 0.9 --ent_coef 0.0 --net_arch "64,64" --reward 4
